3702c989e7903915e8099aa9a2bb5084801c687b
Elinor Huntington <eahuntington@gmail.com>
2016-04-16 15:24:10 -0700

    Pass in command line arg for number of times to run

--- hw1/c/c.c
+++ hw1/c/c.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-#define N 100
 pthread_mutex_t mutex;
 pthread_cond_t condition_consumer, condition_producer;
 int buffer = 0;
 
+struct ptr_struct
+{
+	int M;
+};
+
+
 void *producer(void *ptr){
+	struct ptr_struct *p = (struct ptr_struct *)ptr;
 	int i;
-	for(i = 0; i < N; i++){
+	for(i = 0; i < p->M; i++){
 		pthread_mutex_lock(&mutex);
 		while(buffer !=0){
 			pthread_cond_wait(&condition_producer, &mutex);
@@ -22,8 +28,9 @@ void *producer(void *ptr){
 
 } 
 void *consumer(void *ptr){
+	struct ptr_struct *p = (struct ptr_struct *)ptr;
 	int i;
-	for(i = 0; i < N; i++){
+	for(i = 0; i < p->M; i++){
 		pthread_mutex_lock(&mutex);
 		while(buffer == 0){
 			pthread_cond_wait(&condition_consumer, &mutex);
@@ -36,12 +43,29 @@ void *consumer(void *ptr){
 }
 
 int main(int argc, char **argv){
+	if (argc != 2){
+		printf("Only pass one argument for number of times to run producer-consumer\n");
+		return 1;
+	}
+	char *ptr;
+	long N = strtol(argv[1], &ptr, 10);
+	if(*ptr){
+		printf("And make that argument an integer\n");
+		return 1;
+	}
+	if (N <= 0){
+		printf("greater than zero, please!\n");
+		return 1;
+	}
+
+	struct ptr_struct p;
+	p.M = N;
 	pthread_t prod_thread, cons_thread;
 	pthread_mutex_init(&mutex, 0);
 	pthread_cond_init(&condition_consumer, 0);
 	pthread_cond_init(&condition_producer, 0);
-	pthread_create(&cons_thread, 0, consumer, 0);
-	pthread_create(&prod_thread, 0, producer, 0);
+	pthread_create(&cons_thread, 0, consumer, (void *)&p);
+	pthread_create(&prod_thread, 0, producer, (void *)&p);
 	pthread_join(prod_thread, 0);
 	pthread_join(cons_thread, 0);
 	pthread_cond_destroy(&condition_consumer);
