534a50e4893ab0afc615a6e7e4be1a16ac431bb5
Christine Nguyen <christine.tn.nguyen@gmail.com>
2016-05-01 10:44:27 -0700

    Fixed to work, update includes

--- hw2/3/consumer.c
+++ hw2/3/consumer.c
@@ -1,24 +1,19 @@
-#define _GNU_SOURCE
-#include <stdio.h>			// printf
-#include <stdlib.h>			// exit
-#include <errno.h>			// errno
-#include <fcntl.h>			// 0_CREAT, 0_EXEC
-#include <unistd.h>
-#include <sys/types.h>		// key_t, sem_t
-#include <semaphore.h>		// sem_open, sem_destroy, sem_wait
-#include <sys/stat.h>
-#include <sys/mman.h>
+#include <stdio.h>       // printf
+#include <stdlib.h>      // exit
+#include <errno.h>       // errno
+#include <fcntl.h>       // 0_CREAT, 0_EXEC
+#include <sys/mman.h>    // mmap, etc
+#include <sys/types.h>   // key_t, sem_t
+#include <semaphore.h>   // sem_open, sem_destroy, sem_wait
+#include <sys/stat.h>    // struct st
 
 #define MAX 10
 
 sem_t *sem;  // semaphore
-int fd, *map;
+char *map;   // memory mapped file
 
 int consume() {
-	struct stat st;
 	int i;
-
-	fstat(fd, &st);
 
 	/* Consume items in shared memory */
 	for (i = 0; i < MAX; i++) {
@@ -26,19 +21,12 @@ int consume() {
 		sem_wait(sem);
 
 		/* Consume item */
-		if (ftruncate(fd, st.st_size-1) != 0) {
-			perror("Can't extend file.\n");
-			exit(1);
+		if (map[i] != 'c') {
+			map[i] = 'c';
+			printf("Consumed: %c\n", map[i]);
+		} else {
+			printf("Consumed: NOTHING\n");
 		}
-/*
-		if (map = mremap(map, st.st_size - 1, st.st_size,
-			MREMAP_MAYMOVE) == MAP_FAILED) {
-			perror("Can't remap map.\n");
-			exit(1);
-		}*/
-
-		//printf("Consume: %d\n", map[i]);
-		printf("Consumed: %d\n", i);
 
 		/* Do up on semaphore */
 		sem_post(sem);
@@ -49,6 +37,8 @@ int consume() {
 
 int main(void) {
 	key_t sem_key;      // semaphore key
+	struct stat st;     // struct to file size
+	int fd;             // for opening files
 	time_t start, end;  // used as timer
 	int terminator = 1; // change to 0 in while loop when done
 	double elapsed;     // elapsed time
@@ -58,14 +48,20 @@ int main(void) {
 	sem_unlink("semaphoreV");
 
 	/* Open file for reading/writing */
-	fd = open("sharedfile.bin", O_RDWR, 0666);
+	fd = open("sharedfile.txt", O_RDWR, (mode_t)0666);
 	if (fd == -1) {
 		perror("Could not open file.\n");
 		exit(1);
 	}
 
+	/* Get file stats */
+	if (stat("sharedfile.txt", &st) == -1) {
+		perror("Could not get stats.\n");
+		exit(1);
+	}
+
 	/* Map file */
-	map = mmap(0, MAX * sizeof(int), PROT_READ | PROT_WRITE,
+	map = mmap(0, st.st_size, PROT_READ | PROT_WRITE,
 		MAP_SHARED, fd, 0);
 	if (map == MAP_FAILED) {
 		close(fd);
@@ -86,12 +82,12 @@ int main(void) {
 	}
 
 	/* Sync file changes */
-	if (msync(map, MAX * sizeof(int), MS_SYNC) < 0) {
+	if (msync(map, st.st_size, MS_SYNC) < 0) {
 		perror("Can't sync file.\n");
 	}
 
 	/* Deallocate mapped memory */
-	if (munmap(map, MAX * sizeof(int)) == -1) {
+	if (munmap(map, st.st_size) == -1) {
 		perror("Can't unmap the file.\n");
 	}
 
