f4a2601eded6e1ba0c53330f36b07667e80309dc
Christine Nguyen <christine.tn.nguyen@gmail.com>
2016-05-01 10:44:52 -0700

    Fixed to work, update includes

--- hw2/3/producer.c
+++ hw2/3/producer.c
@@ -1,52 +1,35 @@
-#define _GNU_SOURCE
-#include <stdio.h>			// printf
-#include <stdlib.h>			// exit
-#include <string.h>
-#include <errno.h>			// errno
-#include <fcntl.h>			// 0_CREAT, 0_EXEC
-#include <unistd.h>
-#include <sys/types.h>		// key_t, sem_t
-#include <semaphore.h>		// sem_open, sem_destroy, sem_wait
-#include <sys/stat.h>
-#include <sys/mman.h>
+#include <stdio.h>       // printf
+#include <stdlib.h>      // exit
+#include <errno.h>       // errno
+#include <fcntl.h>       // 0_CREAT, 0_EXEC
+#include <sys/mman.h>    // mmap, etc
+#include <sys/types.h>   // key_t, sem_t
+#include <semaphore.h>   // sem_open, sem_destroy, sem_wait
+#include <sys/stat.h>    // struct st
 
 #define MAX 10
 
 sem_t *sem;  // semaphore
-int fd, *map;
+char *map;   // memory mapped file
 
 int produce() {
-	struct stat st;
-	off_t len_file, len;
-	char src[1] = "p";
 	int i;
 
-	fstat(fd, &st);
-	len_file = st.st_size;
-
 	/* Produce items in memory mapped file */
-	if (len_file < MAX * sizeof(int)) {
-		for (i = 0; i < MAX; i++) {
-			/* Do down on semaphore */
-			sem_wait(sem);
+	for (i = 0; i < MAX; i++) {
+		/* Do down on semaphore */
+		sem_wait(sem);
 
-			len = len_file;
-			len_file += strlen(src);
-
-			if (ftruncate(fd, len_file) != 0) {
-				perror("p - Can't extend file.\n");
-				exit(1);
-			}
-
-			/* Produce item */
-			memcpy(map+len, src, len_file - len);
-
-			//printf("Produce: %d\n", map[i]);
-			printf("Produced: %d\n", i);
-
-			/* Do up on semaphore */
-			sem_post(sem);
+		/* Produce item */
+		if (map[i] != 'p') {
+			map[i] = 'p';
+			printf("Produced: %c\n", map[i]);
+		} else {
+			printf("Produced: NOTHING\n");
 		}
+
+		/* Do up on semaphore */
+		sem_post(sem);
 	}
 
 	return 0;
@@ -54,6 +37,8 @@ int produce() {
 
 int main(void) {
 	key_t sem_key;      // semaphore key
+	struct stat st;     // struct to file size
+	int fd;             // for opening files
 	time_t start, end;  // used as timer
 	int terminator = 1; // change to 0 in while loop when done
 	double elapsed;     // elapsed time
@@ -63,14 +48,20 @@ int main(void) {
 	sem_unlink("semaphoreV");
 
 	/* Open file for reading/writing */
-	fd = open("sharedfile.bin", O_RDWR, 0666);
+	fd = open("sharedfile.txt", O_RDWR, (mode_t)0666);
 	if (fd == -1) {
 		perror("Could not open file.\n");
 		exit(1);
 	}
 
+	/* Get file stats */
+	if (stat("sharedfile.txt", &st) == -1) {
+		perror("Could not get stats.\n");
+		exit(1);
+	}
+
 	/* Map file */
-	map = mmap(0, MAX * sizeof(int), PROT_READ | PROT_WRITE,
+	map = mmap(0, st.st_size, PROT_READ | PROT_WRITE,
 		MAP_SHARED, fd, 0);
 	if (map == MAP_FAILED) {
 		close(fd);
@@ -91,12 +82,12 @@ int main(void) {
 	}
 
 	/* Sync file changes */
-	if (msync(map, MAX * sizeof(int), MS_SYNC) < 0) {
+	if (msync(map, st.st_size, MS_SYNC) < 0) {
 		perror("Can't sync file.\n");
 	}
 
 	/* Deallocate mapped memory */
-	if (munmap(map, MAX * sizeof(int)) == -1) {
+	if (munmap(map, st.st_size) == -1) {
 		perror("Can't unmap the file.\n");
 	}
 
