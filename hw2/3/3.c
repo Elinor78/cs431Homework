e875446bd78b9e0d6f75b0526d66c138259f1299
Christine Nguyen <christine.tn.nguyen@gmail.com>
2016-05-02 04:24:06 -0700

    Add wait for child, check return values of fork

--- hw2/3/3.c
+++ hw2/3/3.c
@@ -23,6 +23,7 @@
 
 int main(void) {
 	pid_t producer, consumer, tpid;
+	int status;    // status of child
 	int semid;     // semaphore id
 	sem_t *sem;    // semaphore
 	key_t sem_key; // semaphore key
@@ -33,19 +34,36 @@ int main(void) {
 	sem_unlink("semaphoreV");
 
 	/* Use fork() and exec() to create producer and consumer processes */
-	if (consumer = fork()) {
-		if (producer = fork()) {
+	consumer = fork()
+	if (consumer == 0 {
+		// Child of main
+		producer = fork()
+		if (producer == 0 {
+			// Child of consumer
 			execv("producer.exe", argv);
-			exit(0);
+			exit(1);
+		} else if (producer > 0) {
+			// Parent of producer
+			execv("consumer.exe", argv);
+			exit(1);
+		} else {
+			perror("Fork failed\n");
+			exit(1);
 		}
-		execv("consumer.exe", argv);
-		exit(0);
-	} else {
+	} else if (consumer > 0) {
+		// Parent of consumer
 		printf("Waiting for producer and consumer to finish.\n");
-		sleep(15);
-		
-		printf("All processes finished.\n");
+	} else {
+		perror("Fork failed\n");
+		exit(1);
 	}
+
+	/* Wait for child processes to finish */
+	while ((wpid = wait(&status)) > 0){
+		printf("Exit status of process %d was %d\n", (int)wpid, status);
+	}
+
+	printf("All processes finished.\n");
 
 	return 0;
 }
