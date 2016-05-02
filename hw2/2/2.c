ffe2952fa6c12413de69f2a5d2abe4a3ce927d4f
Christine Nguyen <christine.tn.nguyen@gmail.com>
2016-05-02 04:13:04 -0700

    Add wait for child, check return values of fork

--- hw2/2/2.c
+++ hw2/2/2.c
@@ -21,7 +21,8 @@
 #define MAX 10
 
 int main(void) {
-	pid_t producer, consumer, tpid;
+	pid_t producer, consumer, wpid;
+	int status;    // status of child
 	int shmid;     // shared memory id
 	int semid;     // semaphore id
 	key_t shm_key; // shared memory key
@@ -39,26 +40,43 @@ int main(void) {
 	}
 
 	/* Create semaphore */
-	sem = sem_open("semaphoreV", O_CREAT | O_EXCL, 0666, 1);
-	sem_unlink("semaphoreV");
+	sem = sem_open("semaphore", O_CREAT | O_EXCL, 0666, 1);
+	sem_unlink("semaphore");
 
 	/* Use fork() and exec() to create producer and consumer processes */
-	if (consumer = fork()) {
-		if (producer = fork()) {
+	consumer = fork();
+	if (consumer == 0) {
+		// Child of main
+		producer = fork();
+		if (producer == 0) {
+			// Child of consumer
 			execv("producer.exe", argv);
-			exit(0);
+			exit(1);
+		} else if (producer > 0) {
+			// Parent of producer
+			execv("consumer.exe", argv);
+			exit(1);
+		} else {
+			perror("Fork failed\n");
+			exit(1);
 		}
-		execv("consumer.exe", argv);
-		exit(0);
-	} else {
+	} else if (consumer > 0) {
+		// Parent of consumer
 		printf("Waiting for producer and consumer to finish.\n");
-		sleep(15);
-
-		/* Deallocate the shared memory segment. */
-		shmctl(shmid, IPC_RMID, 0);
-		
-		printf("All processes finished.\n");
+	} else {
+		perror("Fork failed\n");
+		exit(1);
 	}
+
+	/* Wait for child processes to finish */
+	while ((wpid = wait(&status)) > 0){
+		printf("Exit status of process %d was %d\n", (int)wpid, status);
+	}
+
+	/* Deallocate the shared memory segment. */
+	shmctl(shmid, IPC_RMID, 0);
+	
+	printf("All processes finished.\n");
 
 	return 0;
 }
